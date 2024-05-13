b2a3365ff5e5dada32c76b33a85f8a0bb933bc1d
Aditya <codecommander03@gmail.com>
2024-05-13 22:09:40 +0530

    user and worker signin routes added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,8 @@
 import express from "express";
+import userRouter from "./routers/user";
+import workerRouter from "./routers/user";
+
 const app = express();
 
+app.use("/v1/user", userRouter);
+app.use("/v1/worker", workerRouter);
