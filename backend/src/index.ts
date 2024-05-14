f6852642fbaacc314481fb4c756d4f06bcd5c166
Aditya <codecommander03@gmail.com>
2024-05-14 22:37:49 +0530

    backend update with fake data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,5 +4,9 @@ import workerRouter from "./routers/user";
 
 const app = express();
 
+export const JWT_SECRET = "aditya123";
+
 app.use("/v1/user", userRouter);
 app.use("/v1/worker", workerRouter);
+
+app.listen(3000)
