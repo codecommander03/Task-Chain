36a7373a5a2af95bc3945b4ef8bdab8f7a29f7d0
Aditya <codecommander03@gmail.com>
2024-05-15 21:42:03 +0530

    added worker auth middlware

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from ".";
+import { WORKER_JWT_SECRET } from "./routers/worker";
 
 export function authMiddleware(
     req: Request, res: Response, next: NextFunction) {
@@ -8,6 +9,30 @@ export function authMiddleware(
 
     try {
         const decoded = jwt.verify(authHeader, JWT_SECRET);
+        console.log(decoded);
+        // @ts-ignore
+        if (decoded.userId) {
+            // @ts-ignore
+            req.userId = decoded.userId;
+            return next();
+        } else {
+            return res.status(403).json({
+                message: "You are not logged in",
+            });
+        }
+    } catch (e) {
+        return res.status(403).json({
+            message: "You are not logged in",
+        });
+    }
+}
+
+export function workerMiddleware(
+    req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers["authorization"] ?? "";
+
+    try {
+        const decoded = jwt.verify(authHeader, WORKER_JWT_SECRET);
         console.log(decoded);
         // @ts-ignore
         if (decoded.userId) {
