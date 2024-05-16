1ab2eb91b66825668bd96112e31ba5b70401df1b
Aditya <codecommander03@gmail.com>
2024-05-16 22:37:08 +0530

    created validation for submission post endpoint

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -7,3 +7,8 @@ export const createTaskInput = z.object({
     title: z.string().optional(),
     signature: z.string()
 });
+
+export const createSubmissionInput = z.object({
+    taskId: z.string(),
+    selection: z.string()
+});
