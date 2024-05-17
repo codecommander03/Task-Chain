0a232a2f3cf30a669284a53b9beec638a08c5956
Aditya <codecommander03@gmail.com>
2024-05-17 21:43:45 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import userRouter from "./routers/user";
 import workerRouter from "./routers/user";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 
 app.use("/v1/user", userRouter);
 app.use("/v1/worker", workerRouter);
