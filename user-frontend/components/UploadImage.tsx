8e2eb1c71723c7bdf76d8183539037b9d7e5fa5e
Aditya <codecommander03@gmail.com>
2024-05-18 22:58:29 +0530

    updated upload image component

--- user-frontend/components/UploadImage.tsx
+++ user-frontend/components/UploadImage.tsx
@@ -6,10 +6,16 @@ type Props = {}
 const UploadImage = (props: Props) => {
     const [image, setImage] = useState("");
     return (
-        <div className='w-40 h-40 rounded border text-2xl cursor'>
+        <div className='w-40 h-40 rounded border text-2xl cursor-pointer'>
             <div className='h-full flex justify-center'>
-                <div className='h-full flex justify-center flex-col'>
+                <div className='h-full flex justify-center flex-col relative'>
                     +
+                    <input
+                        className="w-full h-full bg-red-400"
+                        type="file"
+                        style={{ position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%" }}
+                        // onChange={onFileSelect}
+                    />
                 </div>
             </div>
         </div>
