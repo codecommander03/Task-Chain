f4016e2577e7c31fb372a1cf9e4eef05ac94cb4a
Aditya <codecommander03@gmail.com>
2024-05-18 22:50:31 +0530

    appbar component added

---
+++ user-frontend/components/Appbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Appbar = (props: Props) => {
+    return (
+        <main>
+            Appbar
+        </main>
+    )
+}
+
+export default Appbar
