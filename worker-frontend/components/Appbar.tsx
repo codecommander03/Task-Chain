6abbd7bfc08e5c3950345154ef118a809d2ee9f6
Aditya <codecommander03@gmail.com>
2024-05-19 22:29:17 +0530

    appbar for worker

---
+++ worker-frontend/components/Appbar.tsx
@@ -0,0 +1,7 @@
+export const Appbar = () => {
+    return <div className="flex justify-between border-b pb-2 pt-2">
+        <div className="text-2xl pl-4 flex justify-center pt-3">
+            TaskChain (Worker)
+        </div>
+    </div>
+}
