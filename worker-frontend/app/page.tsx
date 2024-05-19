5a7753082bbd05d9ff73594f2ddc26ee4aa1437c
Aditya <codecommander03@gmail.com>
2024-05-19 22:30:04 +0530

    added components

--- worker-frontend/app/page.tsx
+++ worker-frontend/app/page.tsx
@@ -1,9 +1,12 @@
+import { Appbar } from "@/components/Appbar";
+import { NextTask } from "@/components/NextTask";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Appbar />
+      <NextTask />
     </main>
   );
 }
