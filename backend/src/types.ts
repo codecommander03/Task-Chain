aa9b1c75d20aa8bdd3875e27e9b88ac865d19fdc
Aditya <codecommander03@gmail.com>
2024-05-19 22:31:11 +0530

    added minimum limit to options

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 export const createTaskInput = z.object({
     options: z.array(z.object({
         imageUrl: z.string()
-    })),
+    })).min(2),
     title: z.string().optional(),
     signature: z.string()
 });
