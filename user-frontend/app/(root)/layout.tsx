d302f78f639169dd26a21b42362986b10f67d4b1
Aditya <codecommander03@gmail.com>
2024-05-21 21:51:08 +0530

    added solana wallet interface

--- user-frontend/app/(root)/layout.tsx
+++ user-frontend/app/(root)/layout.tsx
@@ -1,36 +1,49 @@
 "use client";
-import React, { FC, useMemo } from 'react';
-// import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-// import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-// import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-// import { clusterApiUrl } from '@solana/web3.js';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React, { FC, useMemo } from "react";
+import {
+    ConnectionProvider,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 
 // Default styles that can be overridden by your app
-// require('@solana/wallet-adapter-react-ui/styles.css');
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-//   const network = WalletAdapterNetwork.Mainnet;
+    children,
+    }: Readonly<{
+    children: React.ReactNode;
+    }>) {
+    const network = WalletAdapterNetwork.Devnet;
 
-  // You can also provide a custom RPC endpoint.
-  const endpoint = "your_rpc_url";
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-//   const wallets = useMemo(
-//       () => [],
-//       [network]
-//   );
+    const wallets = useMemo(
+        () => [],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [network]
+    );
 
     return (
-        <></>
-    // <ConnectionProvider endpoint={endpoint}>
-    //     <WalletProvider wallets={wallets} autoConnect>
-    //         <WalletModalProvider>
-    //             {children}
-    //         </WalletModalProvider>
-    //     </WalletProvider>
-    // </ConnectionProvider>
-  );
+        <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+            {children}
+            </WalletModalProvider>
+        </WalletProvider>
+        </ConnectionProvider>
+    );
 }
