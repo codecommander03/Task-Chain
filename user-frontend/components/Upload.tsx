b35f8021448a3e81ccaae44f63e80aed1b1b9741
Aditya <codecommander03@gmail.com>
2024-05-22 22:38:36 +0530

    added wallet interface

--- user-frontend/components/Upload.tsx
+++ user-frontend/components/Upload.tsx
@@ -1,18 +1,18 @@
 "use client";
-// import { PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import { PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
 import { UploadImage } from "@/components/UploadImage";
 import { BACKEND_URL } from "@/utils";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-// import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
 export const Upload = () => {
     const [images, setImages] = useState<string[]>([]);
     const [title, setTitle] = useState("");
     const [txSignature, setTxSignature] = useState("");
-    // const { publicKey, sendTransaction } = useWallet();
-    // const { connection } = useConnection();
+    const { publicKey, sendTransaction } = useWallet();
+    const { connection } = useConnection();
     const router = useRouter();
 
     async function onSubmit() {
@@ -33,56 +33,56 @@ export const Upload = () => {
 
     async function makePayment() {
 
-        // const transaction = new Transaction().add(
-        //     SystemProgram.transfer({
-        //         fromPubkey: publicKey!,
-        //         toPubkey: new PublicKey("2KeovpYvrgpziaDsq8nbNMP4mc48VNBVXb5arbqrg9Cq"),
-        //         lamports: 100000000,
-        //     })
-        // );
+        const transaction = new Transaction().add(
+            SystemProgram.transfer({
+                fromPubkey: publicKey!,
+                toPubkey: new PublicKey("2KeovpYvrgpziaDsq8nbNMP4mc48VNBVXb5arbqrg9Cq"),
+                lamports: 100000000,
+            })
+        );
 
-        // const {
-        //     context: { slot: minContextSlot },
-        //     value: { blockhash, lastValidBlockHeight }
-        // } = await connection.getLatestBlockhashAndContext();
+        const {
+            context: { slot: minContextSlot },
+            value: { blockhash, lastValidBlockHeight }
+        } = await connection.getLatestBlockhashAndContext();
 
-        // const signature = await sendTransaction(transaction, connection, { minContextSlot });
+        const signature = await sendTransaction(transaction, connection, { minContextSlot });
 
-        // await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
-        // setTxSignature(signature);
+        await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature });
+        setTxSignature(signature);
     }
 
-    return <div className="flex justify-center">
-        <div className="max-w-screen-lg w-full">
-            <div className="text-2xl text-left pt-20 w-full pl-4">
-                Create a task
-            </div>
-
-            <label className="pl-4 block mt-2 text-md font-medium text-gray-900 text-black">Task details</label>
-
-            <input onChange={(e) => {
-                setTitle(e.target.value);
-            }} type="text" id="first_name" className="ml-4 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="What is your task?" required />
-
-            <label className="pl-4 block mt-8 text-md font-medium text-gray-900 text-black">Add Images</label>
-            {/* <div className="flex justify-center pt-4 max-w-screen-lg">
-                {images.map(image => <UploadImage image={image} onImageAdded={(imageUrl) => {
-                    setImages(i => [...i, imageUrl]);
-                }} />)}
-            </div> */}
-
-        <div className="ml-4 pt-2 flex justify-center">
-            <UploadImage onImageAdded={(imageUrl) => {
-                setImages(i => [...i, imageUrl]);
-            }} />
-        </div>
-
+    return (
         <div className="flex justify-center">
-            <button onClick={txSignature ? onSubmit : makePayment} type="button" className="mt-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                {txSignature ? "Submit Task" : "Pay 0.1 SOL"}
-            </button>
-        </div>
-        
-      </div>
-    </div>
+            <div className="max-w-screen-lg w-full">
+                <div className="text-2xl text-left pt-20 w-full pl-4">
+                    Create a task
+                </div>
+
+                <label className="pl-4 block mt-2 text-md font-medium text-gray-900 text-black">Task details</label>
+
+                <input onChange={(e) => {
+                    setTitle(e.target.value);
+                }} type="text" id="first_name" className="ml-4 mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="What is your task?" required />
+
+                <label className="pl-4 block mt-8 text-md font-medium text-gray-900 text-black">Add Images</label>
+                <div className="flex justify-center pt-4 max-w-screen-lg">
+                    {images.map(image => <UploadImage image={image} key={title} onImageAdded={(imageUrl) => {
+                            setImages(i => [...i, imageUrl]);
+                        }} />)}
+                </div>
+
+                <div className="ml-4 pt-2 flex justify-center">
+                    <UploadImage onImageAdded={(imageUrl) => {
+                        setImages(i => [...i, imageUrl]);
+                    }} />
+                </div>
+
+                <div className="flex justify-center">
+                    <button onClick={txSignature ? onSubmit : makePayment} type="button" className="mt-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        {txSignature ? "Submit Task" : "Pay 0.1 SOL"}
+                    </button>
+                </div>
+            </div>
+        </div>);
 }
